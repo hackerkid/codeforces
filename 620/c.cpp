863fb059d387c212b6bc0269f062ca351ddcc07b
Vishnu Ks <hackerkid@vishnuks.com>
2016-01-22 00:48:42 +0530

    educational round c added

---
+++ 620/c.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <stdio.h>
+
+using namespace std;
+
+
+int main()
+{
+
+	int n;
+	int a[300005];
+	int left;
+
+	map <int, bool> mp;
+
+	scanf("%d", &n);
+	
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &a[i]);
+	}	
+	vector < pair <int, int> > vec;
+	left = 1;
+
+	for (int i  = 1; i <= n; i++) {
+		if(mp.find(a[i]) != mp.end()) {
+			vec.push_back(make_pair(left, i));
+			left = i + 1;
+			mp.clear();
+		}
+		else {
+			mp[a[i]] = true;
+		}
+	}
+
+	if(vec.size() > 0 and vec[vec.size() - 1].second != n) {
+		vec[vec.size() - 1].second = n;
+	}
+
+	if(vec.size() != 0) {
+		printf("%ld\n", vec.size());
+		for (int i = 0; i < vec.size(); i++) {
+			printf("%d %d\n", vec[i].first, vec[i].second);
+		}
+	}
+	else {
+		printf("-1\n");
+	}
+
+	return 0;
+
+	
+}
